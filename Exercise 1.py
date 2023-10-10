2f38ab18d932ac2df49aae24b5b1fcfe2190fa8e
josh sicat <sicatjosh2@gmail.com>
2023-10-10 20:31:15 +0400

    Create Exercise 1.py

---
+++ Exercise 1.py
@@ -0,0 +1,7 @@
+name =  input("please enter your name:")
+age= int(input("please enter your age"))
+
+print("Name: ", name)
+print("Age: ", age)
+print("Length of the name: ", (name))
+print("age after on year: ", age + 1)
